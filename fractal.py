3e3692225ef2118e0abe6e5e1e96bdb335eb5a94
SemirBaldzhiev <semir.baldzhiev@gmail.com>
2024-05-21 12:41:46 +0300

    Extract constants in variables

--- fractal.py
+++ fractal.py
@@ -1,26 +1,31 @@
 import turtle
 
-def draw_sierpinski_triangle(turtle, order, size):
-    if order == 0:
+DEGREE = 60
+SIDES = 3
+DRAW_SPEED = 100
+SIZE_COEFFICIENT = 100
+
+def draw_sierpinski_triangle(turtle, level, size):
+    if level == 0:
         turtle.fillcolor("red")
         turtle.begin_fill()
-        for _ in range(3):
+        for _ in range(SIDES):
             turtle.forward(size)
-            turtle.left(120)
+            turtle.left(DEGREE*2)
         turtle.end_fill()
     else:
         size /= 2
-        draw_sierpinski_triangle(turtle, order - 1, size)
+        draw_sierpinski_triangle(turtle, level - 1, size)
         turtle.forward(size)
-        draw_sierpinski_triangle(turtle, order - 1, size)
+        draw_sierpinski_triangle(turtle, level - 1, size)
         turtle.backward(size)
-        turtle.left(60)
+        turtle.left(DEGREE)
         turtle.forward(size)
-        turtle.right(60)
-        draw_sierpinski_triangle(turtle, order - 1, size)
-        turtle.left(60)
+        turtle.right(DEGREE)
+        draw_sierpinski_triangle(turtle, level - 1, size)
+        turtle.left(DEGREE)
         turtle.backward(size)
-        turtle.right(60)
+        turtle.right(DEGREE)
 
 def main():
     window = turtle.Screen()
@@ -30,14 +35,18 @@ def main():
     sierpinski_turtle.penup()
     sierpinski_turtle.setpos(-window.window_width() / 2, -window.window_height() / 2)
     sierpinski_turtle.pendown()
-    sierpinski_turtle.speed(100)
+    sierpinski_turtle.speed(DRAW_SPEED)
 
-    order = int(input("Enter the level of the Sierpiński triangle: "))
-    size = 100*order
+    level = int(input("Enter the level of the Sierpiński triangle [0-6]: "))
+    while (level < 0 or level > 6):
+        print("Invalid level. Please enter a level between 0 and 6.")
+        level = int(input("Enter the level of the Sierpiński triangle [0-6]: "))
+        
+    size = SIZE_COEFFICIENT * level
     
-    if size == 0: size = 100
+    if size == 0: size = SIZE_COEFFICIENT
 
-    draw_sierpinski_triangle(sierpinski_turtle, order, size)
+    draw_sierpinski_triangle(sierpinski_turtle, level, size)
 
     window.exitonclick()
 
